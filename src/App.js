1c86a855c933ab1436d0ea14ee1e8e32a40f0091
YannHyaric <ylhalmeida@gmail.com>
2020-11-14 19:11:09 -0300

    checkout e subtotal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 function App() {
+  
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
@@ -10,6 +34,9 @@ function App() {
           <Route path="/">
             <Header />
             <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </Router>
