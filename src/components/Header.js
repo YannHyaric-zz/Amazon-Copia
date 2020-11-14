4d8f69008f827b203bfd6a63cf209fdf53ea5ab1
YannHyaric <ylhalmeida@gmail.com>
2020-11-14 18:56:01 -0300

    Products

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,13 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
-function Header() {  
+function Header() {
   return (
     <div className="header">
-      
-        <img
-          className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
-      
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -19,34 +17,25 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
-          </div>
-        
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
 
-        
-          <div className="header__option">
-            <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineTwo">& Orders</span>
-          </div>
-        
-        
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
 
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        
-          <div className="header__optionBasket">
-            <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">
-              0
-            </span>
-          </div>
-        
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,5 +7,3 @@ function Home() {
         </div>
     )
 }
-
-export default Home;
