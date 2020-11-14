3bab2ee8eacd1d2ef739689bd6f19f69d4b56a49
YannHyaric <ylhalmeida@gmail.com>
2020-11-14 18:43:31 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
