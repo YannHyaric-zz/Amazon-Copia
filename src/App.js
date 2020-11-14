233a4426723a416eefada1ee7c6c23f5b688bcb2
YannHyaric <ylhalmeida@gmail.com>
2020-11-14 19:15:09 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home";
+import Home from "./components/Home";
+import Header from "./components Header";
+import Login from "./components/Login";
+
 function App() {
   
   useEffect(() => {
